bb9a067cefdc7385a6984d2ed52d344be9b13305
Vladislav Dragushin <maxhc@inbox.ru>
2022-09-01 16:01:21 +0300

    task4 in process

--- main.py
+++ main.py
@@ -2,11 +2,21 @@ import requests
 import os
 
 
+def check_for_redirect(responce):
+    try:
+        pass
+    except requests.HTTPError:
+        raise
+
+
 def main():
     os.makedirs('books', exist_ok=True)
     for i in range(1, 11):
         url = f"https://tululu.org/txt.php?id={i}"
         response = requests.get(url)
+        if response.history:
+            check_for_redirect(response)
+            pass
         response.raise_for_status() 
         filepath = f'books/kniga{i}.txt'
         with open(filepath, 'wb') as file:
