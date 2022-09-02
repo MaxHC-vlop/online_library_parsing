5bceb0e587862630941bfad41e74b045995e6ce8
Vladislav Dragushin <maxhc@inbox.ru>
2022-09-02 15:14:53 +0300

    added check_for_redirect func

--- main.py
+++ main.py
@@ -2,25 +2,33 @@ import requests
 import os
 
 
-def check_for_redirect(responce):
-    try:
-        pass
-    except requests.HTTPError:
-        raise
+URL = 'https://tululu.org/'
+
+
+def check_for_redirect(response):
+    if response.url == URL:
+        raise requests.exceptions.HTTPError
+
+
+def download_books(i):
+    book_url = f"{URL}txt.php?id={i}"
+    response = requests.get(book_url)
+    if response.history:
+        check_for_redirect(response)
+    else:
+        response.raise_for_status()
+        filepath = f'books/kniga{i}.txt'
+        with open(filepath, 'wb') as file:
+            file.write(response.content)
 
 
 def main():
     os.makedirs('books', exist_ok=True)
     for i in range(1, 11):
-        url = f"https://tululu.org/txt.php?id={i}"
-        response = requests.get(url)
-        if response.history:
-            check_for_redirect(response)
+        try:
+            download_books(i)
+        except requests.exceptions.HTTPError:
             pass
-        response.raise_for_status() 
-        filepath = f'books/kniga{i}.txt'
-        with open(filepath, 'wb') as file:
-            file.write(response.content)
 
 if __name__ == '__main__':
     main()
